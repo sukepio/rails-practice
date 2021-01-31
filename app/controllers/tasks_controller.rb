a5670570e63f4b4203c2ab2117ebbc1a9dfd97eb
sukepio <naoto927sukena>
2021-01-31 12:39:30 +0000

    Add task edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-  
+
   def create
     task = Task.new(task_params)
     task.save!
@@ -18,10 +18,17 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
-  
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
   private
-  
+
   def task_params
     params.require(:task).permit(:name, :description)
   end
